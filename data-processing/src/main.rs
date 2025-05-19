11538a2b2be36166061675ba483a8099a149f018
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2025-05-19 16:06:46 +0100

    Solution for 01_06

--- data-processing/src/main.rs
+++ data-processing/src/main.rs
@@ -8,29 +8,31 @@ trait DataPipeline<T> {
     fn filter(&mut self, predicate: fn(&T) -> bool);
 }
 
-struct Validator<T>;
+struct Validator<T> {
+    storage: Vec<T>,
+}
 
 impl<T> Validator<T> {
     fn new() -> Self {
-        todo!()
+        Validator { storage: Vec::new() }   
     }
 }
 
 impl<T> DataPipeline<T> for Validator<T> {
     fn insert(&mut self, item: T) {
-        todo!()
+        self.storage.push(item);
     }
 
     fn insert_many(&mut self, items: impl Iterator<Item = T>) {
-        todo!()
+        self.storage.extend(items);   
     }
 
     fn num_valid(&self) -> u32 {
-        todo!()
+        self.storage.len() as u32
     }
-
+    
     fn filter(&mut self, predicate: fn(&T) -> bool) {
-        todo!()
+        self.storage.retain(predicate);
     }
 }
 
